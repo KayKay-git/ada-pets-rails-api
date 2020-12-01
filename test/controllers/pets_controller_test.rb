3e23002607120a90279254bef5659fca33ba236c
KayKay-git <kayalew@yahoo.com>
2020-12-01 12:02:24 -0500

    added first index action test, passing

--- test/controllers/pets_controller_test.rb
+++ test/controllers/pets_controller_test.rb
@@ -1,9 +1,17 @@
 require "test_helper"
 
 describe PetsController do
-  it "must get index" do
-    get pets_index_url
-    must_respond_with :success
-  end
+  describe "index" do
+    it "must get index" do
+      get pets_path
+      must_respond_with :success
+    end
 
+    it "responds with JSON and success" do
+      get pets_path
+
+      expect(response.header['Content-Type']).must_include 'json'
+      must_respond_with :ok
+    end
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,13 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
-
+require "minitest/rails"
+require "minitest/reporters"
+Minitest::Reporters.use!(
+    Minitest::Reporters::SpecReporter.new,
+    ENV,
+    Minitest.backtrace_filter
+)
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
