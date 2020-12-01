4455572e8bc64241782f43d33921af0178466162
KayKay-git <kayalew@yahoo.com>
2020-12-01 14:49:06 -0500

    added show method, with tests, passing

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,7 +1,24 @@
 class PetsController < ApplicationController
   def index
-    @pets = Pet.all.order(:name)
+    # @pets = Pet.all.order(:name)
+    # Notice, I dont need an @ symbol bc no view to share it with.
+    pets = Pet.all.order(:name) # putting .as_json(only: [:id, :name, :age, :owner, :species]) passes when done here also.
+    render json: pets.as_json(only: [:id, :name, :age, :owner, :species]), status: :ok
+    # or exclude --> render json: pets.as_json(except: [:created_at, :updated_at]), status: :ok
 
-    render json: { ready_for_lunch: "yessss" }, status: :ok
+  end
+
+  def show
+    pet = Pet.find_by(id: params[:id])
+
+    if pet.nil?
+      render json: {
+          ok: false,
+          message: 'Not Found'    # message is case sensitive, so know that when testing it must match exactly.
+      }, status: :not_found
+      return
+    end
+
+    render json: pet.as_json(only: [:id, :name, :age, :owner, :species]), status: :ok
   end
 end
--- test/controllers/pets_controller_test.rb
+++ test/controllers/pets_controller_test.rb
@@ -1,17 +1,94 @@
-require "test_helper"
+require 'test_helper'
 
 describe PetsController do
-  describe "index" do
-    it "must get index" do
+  required_pet_attrs = ['id', 'name', 'species', 'age', 'owner']
+
+  describe 'index' do
+    it 'must get index' do
       get pets_path
       must_respond_with :success
     end
 
-    it "responds with JSON and success" do
+    it 'responds with JSON and success' do
       get pets_path
 
       expect(response.header['Content-Type']).must_include 'json'
       must_respond_with :ok
     end
+
+    # it 'will return all the proper fields for a list of pets ' do
+    #   pet_fields = ["id", "name", "species", "age", "owner"].sort
+    # end
+
+    it 'responds with an array of pet hashes' do
+      # Act
+      get pets_path
+
+      # Get the body of the response as an array or a hash
+      body = JSON.parse(response.body)
+
+      # Assert
+      expect(body).must_be_instance_of Array
+      body.each do |pet|
+        expect(pet).must_be_instance_of Hash
+
+        required_pet_attrs = ['id', 'name', 'species', 'age', 'owner']
+
+        expect(pet.keys.sort).must_equal required_pet_attrs.sort
+      end
+    end
+
+    it 'will respond with an empty array when there are no pets' do
+      # Arrange
+      Pet.destroy_all
+
+      # Act
+      get pets_path
+      body = JSON.parse(response.body)
+
+      # Assert
+      expect(body).must_be_instance_of Array
+      expect(body).must_equal []
+      expect(body.length).must_equal 0
+    end
+
+
+  end
+
+  describe 'show' do
+
+    # nominal
+    it 'should return a hash with proper fields for an existing pet' do
+      required_pet_attrs = ['id', 'name', 'species', 'age', 'owner'].sort
+
+      pet = pets(:honey)
+
+      # Act
+      get pet_path(pet.id)
+
+      body = JSON.parse(response.body)
+
+      must_respond_with :ok
+      expect(response.header['Content-Type']).must_include 'json'
+
+      expect(body).must_be_instance_of Hash
+      expect(body.keys.sort).must_equal required_pet_attrs.sort
+
+    end
+
+    # edge case
+    it 'should respond with not_found if no pet matches the id' do
+
+      # Act
+      get pet_path(-1)
+
+      must_respond_with :not_found
+      body = JSON.parse(response.body)
+
+      # Then you can test for the json in the body of the response
+      expect(body['ok']).must_equal false
+      expect(body['message']).must_include 'Not Found'
+    end
+
   end
 end
